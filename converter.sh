26bd3605aece25020f19e93bf7741d41d0c22342
Seth Gower <sethzerish@gmail.com>
2018-06-04 12:17:04 -0400

    Changes pattern matching to subdirectory wildcard

--- converter.sh
+++ converter.sh
@@ -9,14 +9,10 @@ COL_BLUE=$ESC_SEQ"34;01m"
 COL_MAGENTA=$ESC_SEQ"35;01m"
 COL_CYAN=$ESC_SEQ"36;01m"
 
-cd ../Scrubs/input
-for file in *; do
+cd $1
+
+for file in **/*.avi; do
     echo -e "$COL_CYAN Converting: $COL_RESET" $file
-    if [[ ${file: -4} == ".mp4" ]]; then
-        echo ffmpeg -i $file -c:v copy -c:a aac -metadata:s:a:0 language=eng "../output/$file"
-        ffmpeg -i "$file" -c:v copy -c:a aac -metadata:s:a:0 language=eng -strict -2 "../output/$file"
-    else 
-        ffmpeg -i "$file" -c:v libx264 -c:a aac -metadata:s:a:0 language=eng -strict -2 "../output/${file:0:-4}.mp4"
-    fi
-    #rm "$file"
+    ffmpeg -i $file -c:v libx264 -c:a aac -metadata:s:a:0 language=eng $(file//.avi/.mp4)
+    rm $file
 done
