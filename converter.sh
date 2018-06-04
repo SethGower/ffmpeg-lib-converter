00378d04bedd1303713cfef5e9cc37b748df6a35
Seth Gower <sethzerish@gmail.com>
2018-06-04 15:49:57 -0400

    adds mkv to mp4 conversion.

--- converter.sh
+++ converter.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 # Colors
 ESC_SEQ="\x1b["
 COL_RESET=$ESC_SEQ"39;49;00m"
@@ -10,9 +10,14 @@ COL_MAGENTA=$ESC_SEQ"35;01m"
 COL_CYAN=$ESC_SEQ"36;01m"
 
 cd $1
-
 for file in **/*.avi; do
     echo -e "$COL_CYAN Converting: $COL_RESET" $file
-    ffmpeg -i "$file" -c:v libx264 -c:a aac -metadata:s:a:0 language=eng -strict -2 "$(file//.avi/.mp4)"
-    rm $file
+    ffmpeg -i "$file" -c:v libx264 -c:a aac -metadata:s:a:0 language=eng -strict -2 "${file:0:-4}.mp4"
+    rm "$file"
+done
+
+for file in **/*.mkv; do
+    echo -e "$COL_CYAN Converting: $COL_RESET" $file
+    ffmpeg -i "$file" -c:v copy -c:a aac -metadata:s:a:0 language=eng -strict -2 "${file:0:-4}.mp4"
+    rm "$file"
 done
